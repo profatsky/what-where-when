a6c50f2049e23886e5c00419f592a7bbe4641a35
Mikhail Profatsky <mixail.rostov04@gmail.com>
2022-09-18 23:36:53 +0300

    updated vk_api dataclasses

--- app/store/vk_api/dataclasses.py
+++ app/store/vk_api/dataclasses.py
@@ -2,16 +2,37 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Action:
+    type: str
+    label: str
+    payload: str
+
+
+@dataclass
+class Button:
+    action: Action
+
+
+@dataclass
+class Keyboard:
+    one_time: bool
+    buttons: list[list[Button]]
+
+
+@dataclass
 class Message:
-    user_id: int
+    peer_id: int
     text: str
+    keyboard: Keyboard | None
 
 
 @dataclass
 class UpdateObject:
     id: int
     user_id: int
+    peer_id: int
     body: str
+    event_type: str
 
 
 @dataclass
